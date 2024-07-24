1b7d3da583b053d9ffb59cee2d52b5c86d2aaf4e
Anton <anton.covu@gmail.com>
2024-07-25 01:29:40 +0200

    nom, formulaire de connexion, sauvegarde et chargement de l'emplacement précèdent, le tout en base de données MongoDB.

--- jeu.js
+++ jeu.js
@@ -1,32 +1,260 @@
+class LoginScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'LoginScene' });
+    }
+
+    create() {
+        this.add.text(640, 200, 'Bienvenue', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
+
+        const nameInput = this.add.dom(640, 300, 'input', {
+            type: 'text',
+            placeholder: 'Entrez votre nom',
+            style: 'width: 200px; padding: 10px;'
+        }).setOrigin(0.5);
+
+        const playButton = this.add.text(640, 400, 'Jouer', { fontSize: '24px', fill: '#0f0' })
+            .setOrigin(0.5)
+            .setInteractive()
+            .on('pointerdown', () => {
+                const name = nameInput.node.value;
+                if (name) {
+                    // check si le nom est déjà utilisé
+                    fetch('http://localhost:3000/checkName/'+name, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    }).then(response => {
+                        if (!response.ok) {
+                            throw new Error('Erreur lors de la vérification du nom');
+                        }
+                        return response.json();
+                    }).then(data => {
+                        if (data.exists) {
+                            this.loginCharacter(name);
+                        }
+                        else {
+                            this.createCharacter(name);
+                        }
+                    }).catch(error => {
+                        console.error('Erreur:', error);
+                        this.add.text(640, 450, 'Erreur: Impossible de vérifier le nom', { fontSize: '16px', fill: '#f00' }).setOrigin(0.5);
+                    });
+
+                } else {
+                    this.add.text(640, 450, 'Veuillez entrer un nom', { fontSize: '16px', fill: '#f00' }).setOrigin(0.5);
+                }
+            });
+    }
+
+    loginCharacter(name) {
+        fetch('http://localhost:3000/getCharacter/'+name, { method: 'GET' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la récupération du personnage');
+            }
+            return response.json();
+        }).then(data => {
+            console.log('Personnage récupéré:', data);
+            this.scene.start('MainScene', { playerName: name, characterId: data._id });
+        }).catch(error => {
+            console.error('Erreur:', error);
+            this.add.text(640, 450, 'Erreur: Impossible de récupérer le personnage', { fontSize: '16px', fill: '#f00' }).setOrigin(0.5);
+        });
+    }
+
+    createCharacter(name) {
+        fetch('http://localhost:3000/createCharacter', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name }),
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la création du personnage');
+            }
+            return response.json();
+        }).then(data => {
+            this.scene.start('MainScene', { playerName: name, characterId: data.characterId });
+        }).catch(error => {
+            console.error('Erreur:', error);
+            this.add.text(640, 450, 'Erreur: Impossible de créer le personnage', { fontSize: '16px', fill: '#f00' }).setOrigin(0.5);
+        });
+    }
+}
+
+
+class MainScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MainScene' });
+    }
+
+    init(data) {
+        this.playerName = data.playerName;
+        this.characterId = data.characterId;
+        this.player = null;
+        this.targetX = 0;
+        this.targetY = 0;
+        this.moving = false;
+        this.speed = 160;
+        this.players = {};
+        this.cursorText = null;
+    }
+
+    preload() {
+        this.load.image('background', 'background.png');
+        this.load.image('ground', 'https://examples.phaser.io/assets/sprites/platform.png');
+        this.load.image('star', 'https://examples.phaser.io/assets/sprites/star.png');
+        this.load.spritesheet('dude', 'https://examples.phaser.io/assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    create() {
+        this.add.image(640, 360, 'background');
+
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(640, 720, 'ground').setScale(2).refreshBody();
+
+        //logique de vérification si x et y existent pour le joueur afin de charger son précedent emplacement sinon on charge les coordonnées par défaut
+        fetch('http://localhost:3000/getPlayerPosition/'+this.characterId, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            if (!response.ok) {
+                this.player = this.physics.add.sprite(688, 231, 'dude');
+                throw new Error('Erreur lors de la récupération de la position du joueur');
+            }
+            return response.json();
+        })
+        .then(data => {
+            this.player = this.physics.add.sprite(data.x || 688, data.y || 231, 'dude');
+            this.player.setCollideWorldBounds(false);
+            this.physics.add.collider(this.player, platforms);
+        });
+
+        this.input.on('pointerdown', (pointer) => {
+            this.targetX = pointer.x;
+            this.targetY = pointer.y;
+            this.moving = true;
+        });
+
+        this.cursorText = this.add.text(10, 10, '', { font: '16px Courier', fill: '#000000' });
+
+        socket.emit('register', { name: this.playerName });
+
+        socket.on('registrationSuccess', (data) => {
+            console.log(`Enregistrement réussi pour ${data.name} avec l'ID de personnage ${data.characterId}`);
+            this.joinGame();
+        });
+
+        socket.on('registrationError', (error) => {
+            console.error('Erreur lors de l\'enregistrement:', error.message);
+        });
+
+        socket.on('playerJoined', (playerData) => {
+            this.addNewPlayerToGame(playerData);
+        });
+
+        socket.on('playerLeft', (playerId) => {
+            this.removePlayerFromGame(playerId);
+        });
+
+        socket.on('playerUpdated', (playerData) => {
+            this.updatePlayerInGame(playerData);
+        });
+    }
+
+    joinGame() {
+        socket.emit('joinGame');
+        socket.emit('newPlayer', {
+            x: this.player.x,
+            y: this.player.y,
+            name: this.playerName
+        });
+    }
+
+    update() {
+        const pointer = this.input.activePointer;
+        this.cursorText.setText(`X: ${pointer.worldX} Y: ${pointer.worldY}`);
+
+        if (this.moving) {
+            const distanceX = this.targetX - this.player.x;
+            const distanceY = this.targetY - this.player.y;
+
+            const angle = Math.atan2(distanceY, distanceX);
+            const velocityX = Math.cos(angle) * this.speed;
+            const velocityY = Math.sin(angle) * this.speed;
+
+            this.player.setVelocity(velocityX, velocityY);
+
+            if (Math.abs(distanceX) < 4 && Math.abs(distanceY) < 4) {
+                this.player.setVelocity(0, 0);
+                this.player.x = this.targetX;
+                this.player.y = this.targetY;
+                this.moving = false;
+
+                this.savePlayerPosition(socket.id);
+            }
+
+            socket.emit('updatePlayer', {
+                id: socket.id,
+                x: this.player.x,
+                y: this.player.y
+            });
+        }
+    }
+
+    savePlayerPosition(characterId) {
+        fetch('http://localhost:3000/savePlayerPosition', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ characterId: this.characterId, x: this.player.x, y: this.player.y }),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la sauvegarde de la position du joueur');
+            }
+            return response.text();
+        })
+        .then(data => {
+            console.log('Position du joueur sauvegardée avec succès:', data);
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+        });
+    }
+
+    addNewPlayerToGame(playerData) {
+        const newPlayer = this.physics.add.sprite(playerData.x, playerData.y, 'dude');
+        newPlayer.setTint(playerData.color);
+        newPlayer.playerId = playerData.id;
+        this.players[playerData.id] = newPlayer;
+    }
+
+    removePlayerFromGame(playerId) {
+        if (this.players[playerId]) {
+            this.players[playerId].destroy();
+            delete this.players[playerId];
+        }
+    }
+
+    updatePlayerInGame(playerData) {
+        if (this.players[playerData.id]) {
+            this.players[playerData.id].setPosition(playerData.x, playerData.y);
+        }
+    }
+}
+
+
 const port = 3000;
 
-let numberOfPlayers = 1;
+let numberOfPlayers = 0;
 let players = {};
-
-const config = {
-    type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: true
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update,
-    }
-};
-
-const game = new Phaser.Game(config);
-
-let player;
-let targetX, targetY;
-let moving = false;
-const speed = 160; // Vitesse du personnage
 
 const socket = io('http://localhost:' + port);
 
@@ -36,136 +264,27 @@ socket.on("connect_error", (err) => {
 
 socket.on("connect", () => {
     console.log('Connecté au serveur');
+    numberOfPlayers++;
 });
 
-function preload() {
-    this.load.image('background', 'background.png');
-    this.load.image('ground', 'https://examples.phaser.io/assets/sprites/platform.png');
-    this.load.image('star', 'https://examples.phaser.io/assets/sprites/star.png');
-    this.load.spritesheet('dude', 'https://examples.phaser.io/assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
-}
 
-function create() {
-
-    // Ajoute l'image de fond
-    this.add.image(640, 360, 'background');
-
-    const platforms = this.physics.add.staticGroup(); // Crée un groupe de plateformes statiques
-    // Créer une plateforme non traversable
-    platforms.create(640, 720, 'ground').setScale(2).refreshBody();
-
-    player = this.physics.add.sprite(688, 231, 'dude');
-    socket.emit('newPlayer', {
-        x: 688,
-        y: 231,
-        // autres données du personnage
-    });
-
-    player.setCollideWorldBounds(false); // Désactive la collision avec les bords du monde
-    this.physics.add.collider(player, platforms); // Active la collision entre le joueur et les plateformes
-
-    this.input.on('pointerdown', function (pointer) {
-        targetX = pointer.x;
-        targetY = pointer.y;
-        moving = true;
-    });
-
-    cursorText = this.add.text(10, 10, '', { font: '16px Courier', fill: '#000000' });
-
-    // Quand un nouveau joueur rejoint
-    socket.on('playerJoined', (playerData) => {
-        numberOfPlayers++;
-        this.addNewPlayerToGame(playerData);
-    });
-
-    // Quand un joueur quitte
-    socket.on('playerLeft', (playerId) => {
-        this.removePlayerFromGame(playerId);
-    });
-
-    // Quand un joueur est mis à jour
-    socket.on('playerUpdated', (playerData) => {
-        this.updatePlayerInGame(playerData);
-        if (players[playerData.id]) {
-            players[playerData.id].setPosition(playerData.x, playerData.y);
+const config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    parent: 'game-container',
+    dom: {
+        createContainer: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
         }
-    });
-}
-
-function update() {
-    const pointer = this.input.activePointer;
-    cursorText.setText(`X: ${pointer.worldX} Y: ${pointer.worldY}`);
-
-    if (moving) {
-        const distanceX = targetX - player.x;
-        const distanceY = targetY - player.y;
-
-        const angle = Math.atan2(distanceY, distanceX);
-        const velocityX = Math.cos(angle) * speed;
-        const velocityY = Math.sin(angle) * speed;
-
-        player.setVelocity(velocityX, velocityY);
-
-        if (Math.abs(distanceX) < 4 && Math.abs(distanceY) < 4) {
-            player.setVelocity(0, 0);
-            player.x = targetX;
-            player.y = targetY;
-            moving = false;
-
-            // Sauvegarde de la position du joueur vers le backend
-            fetch('http://localhost:' + port + '/savePlayerPosition', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ x: player.x, y: player.y }),
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Erreur lors de la sauvegarde de la position du joueur');
-                    }
-                    return response.text();
-                })
-                .then(data => {
-                    console.log('Position du joueur sauvegardée avec succès:', data);
-                })
-                .catch(error => {
-                    console.error('Erreur:', error);
-                });
-        }
-
-        socket.emit('updatePlayer', {
-            id: player.playerId,
-            x: player.x,
-            y: player.y
-        });
-
-    }
-}
-
-Phaser.Scene.prototype.addNewPlayerToGame = function (playerData) {
-    // Create a new player sprite
-    const newPlayer = this.physics.add.sprite(playerData.x, playerData.y, 'dude');
-    console.log(players);
-    // Set the player's properties based on playerData
-    newPlayer.setTint(playerData.color);
-    newPlayer.playerId = playerData.id;
-
-    // Add the new player to the players object
-    players[playerData.id] = newPlayer;
+    },
+    scene: [LoginScene, MainScene]
 };
 
-Phaser.Scene.prototype.removePlayerFromGame = function (playerId) {
-    if (players[playerId]) {
-        players[playerId].destroy();
-        delete players[playerId];
-    }
-};
-
-Phaser.Scene.prototype.updatePlayerInGame = function (playerData) {
-    if (players[playerData.id]) {
-        players[playerData.id].x = playerData.x;
-        players[playerData.id].y = playerData.y;
-    }
-};
+const game = new Phaser.Game(config);
 
--- server.js
+++ server.js
@@ -8,11 +8,31 @@ const io = require('socket.io')(http, {
     origin: '*',
   },
 });
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
+const { get } = require('http');
 
-// Connexion à la base de données
-const dbUri = 'mongodb://localhost:27017/'; // URL de connexion à MongoDB
-const dbName = 'phaserGame'; // Nom de ta base de données
+let mongoClient, db;
+
+// Connexion à la base de données MongoDB
+async function connectToMongo() {
+  const dbUri = 'mongodb://127.0.0.1:27017/';
+  const dbName = 'phaserGame';
+  const client = new MongoClient(dbUri);
+
+  try {
+    await client.connect();
+    console.log('Connexion à MongoDB réussie !');
+
+    const db = client.db(dbName);
+    await db.command({ ping: 1 });
+    console.log("Ping à la base de données réussi !");
+    return { client, db };
+
+  } catch (err) {
+    console.error('Erreur de connexion à MongoDB:', err);
+    throw err;
+  }
+}
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -24,15 +44,21 @@ const players = new Map();
 io.on('connection', (socket) => {
     numberOfPlayers++;
     console.log(`Un joueur s'est connecté, nombre de joueurs: ${numberOfPlayers}`);
-    socket.on('newPlayer', (playerData) => {
-      players.set(socket.id, playerData);
-      socket.broadcast.emit('playerJoined', { id: socket.id, ...playerData });
-      
-      Object.keys(players).forEach(playerId => {
-        if (playerId !== socket.id) {
-          socket.emit('playerJoined', { id: playerId, ...players[playerId] });
-        }
-      });
+    socket.on('newPlayer', async (playerData) => {
+      try {
+        const characterId = await addCharacter({ name: playerData.name });
+        playerData.characterId = characterId;
+        players.set(socket.id, playerData);
+        socket.broadcast.emit('playerJoined', { id: socket.id, ...playerData });
+        
+        players.forEach((data, playerId) => {
+          if (playerId !== socket.id) {
+            socket.emit('playerJoined', { id: playerId, ...data });
+          }
+        });
+      } catch (err) {
+        console.error('Erreur lors de la création du personnage :', err);
+      }
     });
   
     socket.on('disconnect', () => {
@@ -40,28 +66,131 @@ io.on('connection', (socket) => {
         console.log(`Un joueur s'est déconnecté, nombre de joueurs: ${numberOfPlayers}`);
         players.delete(socket.id);
         io.emit('playerLeft', socket.id);
-        socket.broadcast.emit('playerLeft', socket.id);
     });
   
     socket.on('updatePlayer', (playerData) => {
-      players[socket.id] = { ...players[socket.id], ...playerData };
+      const existingPlayer = players.get(socket.id) || {};
+      players.set(socket.id, { ...existingPlayer, ...playerData });
       socket.broadcast.emit('playerUpdated', { id: socket.id, ...playerData });
     });
-    
-  });
+});
 
+async function addCharacter(characterData) {
+  try {
+    const result = await db.collection('players').insertOne({
+      name: characterData.name,
+      level: 1,  // On commence au niveau 1 par défaut
+      createdAt: new Date()
+    });
+    console.log(`Nouveau personnage ajouté avec l'ID : ${result.insertedId} et le nom : ${characterData.name}`);
+    return result.insertedId;
+  } catch (err) {
+    console.error('Erreur lors de l\'ajout du personnage :', err);
+    throw err;
+  }
+}
 
-// Variable pour stocker la position du joueur (exemple simplifié)
-let savedPosition = { x: 0, y: 0 };
+app.post('/createCharacter', async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(400).send('Le nom du personnage est requis');
+    }
+    const characterId = await addCharacter({ name });
+    res.status(201).json({ message: 'Personnage créé avec succès', characterId , name });
+  } catch (err) {
+    res.status(500).send('Erreur lors de la création du personnage');
+  }
+});
 
 // Endpoint pour sauvegarder la position du joueur
-app.post('/savePlayerPosition', (req, res) => {
-    const { x, y } = req.body;
-    savedPosition = { x, y };
-    res.send(`Position du joueur sauvegardée avec succès: x=${x}, y=${y}`);
+app.post('/savePlayerPosition', async (req, res) => {
+    const { x, y, characterId } = req.body;
+    try {
+        await db.collection('playerPositions').updateOne(
+            { playerId: characterId },
+            { $set: { x, y } },
+            { upsert: true }
+        );
+        res.send(`Position du joueur sauvegardée avec succès: x=${x}, y=${y}`);
+    } catch (err) {
+        console.error('Erreur lors de la sauvegarde de la position:', err);
+        res.status(500).send('Erreur lors de la sauvegarde de la position');
+    }
+});
+
+app.get('/getPlayerPosition/:id', async (req, res) => {
+    const characterId = req.params.id;
+    try {
+        const playerPosition = await db.collection('playerPositions').findOne({ playerId: characterId });
+        if (!playerPosition) {
+            return res.status(404).send('Position du joueur non trouvée');
+        }
+        res.json(playerPosition);
+    } catch (err) {
+        console.error('Erreur lors de la récupération de la position du joueur:', err);
+        res.status(500).send('Erreur lors de la récupération de la position du joueur');
+    }
+});
+
+app.get('/getCharacter/:name', async (req, res) => {
+    const name = req.params.name;
+    try {
+        const character = await getCharacter(name);
+        if (!character) {
+            return res.status(404).send('Personnage non trouvé');
+        }
+        console.log('Personnage récupéré :', character);
+        res.json(character);
+    } catch (err) {
+        console.error('Erreur lors de la récupération du personnage :', err);
+        res.status(500).send('Erreur lors de la récupération du personnage');
+    }
+});
+
+async function getCharacter(name) {
+  try {
+    const character = await db.collection('players').findOne({ name });
+    return character;
+  } catch (err) {
+    console.error('Erreur lors de la récupération du personnage :', err);
+    throw err;
+  }
+}
+
+app.get('/checkName/:name', async (req, res) => {
+  try {
+    const name = req.params.name;
+    const character = await db.collection('players').findOne({ name });
+    res.json({ exists: !!character });
+  } catch (err) {
+    res.status(500).send('Erreur lors de la vérification du nom');
+  }
+});
+
+// Fonction pour démarrer le serveur
+async function startServer() {
+  try {
+    ({ client: mongoClient, db } = await connectToMongo());
+    
+    http.listen(PORT, () => {
+      console.log(`Serveur en écoute sur le port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Impossible de démarrer le serveur:", err);
+    process.exit(1);
+  }
+}
+
+// Gestionnaire pour fermer proprement la connexion MongoDB
+process.on('SIGINT', async () => {
+  console.log('Arrêt du serveur...');
+  if (mongoClient) {
+    await mongoClient.close();
+    console.log('Connexion MongoDB fermée.');
+  }
+  process.exit(0);
 });
 
 const PORT = 3000;
-http.listen(PORT, () => {
-  console.log(`Serveur en écoute sur le port ${PORT}`);
-});
+startServer();
